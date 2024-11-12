e5995ee08949c44bcd5677d7778a8ff586d1e819
Tom <83677440+TomMc608@users.noreply.github.com>
2024-11-12 15:26:32 +1300

    update

--- src/CsvUploader.js
+++ src/CsvUploader.js
@@ -128,7 +128,7 @@ const CsvUploader = () => {
     setResults(null);
 
     try {
-      const response = await axios.post("https://school-ai-backend.onrender.com/process", {
+      const response = await axios.post("http://100.20.92.101/process", {
         data: csvData,
         selected_columns: selectedColumns,
       });
